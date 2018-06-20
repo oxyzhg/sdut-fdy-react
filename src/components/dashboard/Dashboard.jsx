f2a8a5f47dd3b5f1c34176ff6dedc8a2dfd0d387
oxyzhg <oxyzhg@gmial.com>
2018-06-20 18:38:25 +0800

    2018-06-20

--- src/components/dashboard/Dashboard.jsx
+++ src/components/dashboard/Dashboard.jsx
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
+import '../../assets/plugin/animate.min.css'
 
 class Dashboard extends Component {
   render() {
     return (
       <div className="main-content">
-        <p><b>Dashboard</b></p>
-        <p>Waiting for more !</p>
+        <p className="animated bounceInUp" style={{ fontSize: '1.5em', fontWeight: 700}}>山东理工大学辅导员风采展示</p>
+        <p className="animated infinite flash">点击左侧导航栏查看详情！</p>
       </div>
     );
   }
